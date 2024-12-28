aa04f5a99f5ff6b724833e435909e74ac4995f77
许世越 <1531845081@qq.com>
2024-12-28 21:03:38 +0800

    update

---
+++ arena_hard.sh
@@ -0,0 +1,7 @@
+python gen_answer.py
+
+python gen_judgment.py
+
+python add_markdown_info.py --dir data/arena-hard-v0.1/model_answer --output-dir data/arena-hard-v0.1/model_answer
+
+python show_result.py --output --style-control --judge-name "deepseek-chat"
